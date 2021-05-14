691d47cad3d70c1a4ee2708bb58e390a60efca34
Domán Attila Rácz <racz.doman@gmail.com>
2021-05-14 20:15:18 +0200

    Add analytics

--- app/src/main/java/com/moblab/cnn_news/ui/news/NewsActivity.kt
+++ app/src/main/java/com/moblab/cnn_news/ui/news/NewsActivity.kt
@@ -2,31 +2,29 @@ package com.moblab.cnn_news.ui.news
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import com.moblab.cnn_news.R
 import com.moblab.cnn_news.injector
 import com.moblab.cnn_news.interactor.news.NewsInteractor
-import com.moblab.cnn_news.interactor.news.event.GetNewsEvent
 import com.moblab.cnn_news.ui.about.AboutActivity
 import com.moblab.cnn_news.ui.news.adapter.NewsAdapter
 import com.moblab.cnn_news.ui.utils.hide
 import com.moblab.cnn_news.ui.utils.show
 import io.swagger.client.model.NewsDetails
 import kotlinx.android.synthetic.main.activity_main.*
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
-import java.util.concurrent.Executor
 import javax.inject.Inject
+
 
 class NewsActivity : AppCompatActivity(), NewsScreen {
 
@@ -41,16 +39,18 @@ class NewsActivity : AppCompatActivity(), NewsScreen {
     private var newsAdapter: NewsAdapter? = null
 
     private val displayedNews: MutableList<NewsDetails> = mutableListOf()
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         injector.inject(this)
+        firebaseAnalytics = Firebase.analytics
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
-        toggle = ActionBarDrawerToggle(this,drawerLayout , R.string.open, R.string.close)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
@@ -58,8 +58,8 @@ class NewsActivity : AppCompatActivity(), NewsScreen {
 
         navView.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_home -> drawerLayout.closeDrawer(GravityCompat.START)
-                R.id.nav_about -> Intent(this,AboutActivity::class.java).also { startActivity(it) }
+                R.id.nav_home -> drawerLayout.closeDrawer(GravityCompat.START) //throw Exception("jajj") for crash analytics
+                R.id.nav_about -> Intent(this, AboutActivity::class.java).also { startActivity(it) }
             }
             true
         }
@@ -94,6 +94,9 @@ class NewsActivity : AppCompatActivity(), NewsScreen {
 
     override fun onResume() {
         super.onResume()
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, "NewsActivity")
+        }
     }
 
     override fun showNetworkError(errorMsg: String) {
@@ -118,4 +121,5 @@ class NewsActivity : AppCompatActivity(), NewsScreen {
             tvEmpty.hide()
         }
     }
+
 }
